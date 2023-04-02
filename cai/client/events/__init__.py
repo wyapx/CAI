0b60ab87e3aa9da7becec2bf2f2eefc64a9dd49e
a1025 <a1025952421@outlook.com>
2023-04-02 17:06:01 +0800

    chore(events): export events

--- cai/client/events/__init__.py
+++ cai/client/events/__init__.py
@@ -1,6 +1,7 @@
 from .base import Event as Event
 from .group import GroupEvent as GroupEvent
 from .common import NudgeEvent as NudgeEvent
+from .common import OtherClientChangedEvent as OtherClientChangedEvent
 from .group import GroupRedbagEvent as GroupRedbagEvent
 from .group import GroupMemberMutedEvent as GroupMemberMutedEvent
 from .group import GroupNameChangedEvent as GroupNameChangedEvent
@@ -8,6 +9,18 @@ from .group import GroupMemberUnMutedEvent as GroupMemberUnMutedEvent
 from .group import GroupMessageRecalledEvent as GroupMessageRecalledEvent
 from .group import (
     GroupMemberSpecialTitleChangedEvent as GroupMemberSpecialTitleChangedEvent,
+    GroupLuckyCharacterChangedEvent,
+    GroupLuckyCharacterClosedEvent,
+    GroupLuckyCharacterOpenedEvent,
+    GroupLuckyCharacterInitEvent,
+    GroupLuckyCharacterNewEvent,
+    GroupMemberJoinedEvent,
+    GroupMemberUnMutedEvent,
+    GroupMemberLeaveEvent,
+    GroupMemberPermissionChangeEvent,
+    JoinGroupRequestEvent,
+    TransferGroupEvent,
+    GroupNudgeEvent
 )
 
 __all__ = [
@@ -19,4 +32,17 @@ __all__ = [
     "GroupMemberSpecialTitleChangedEvent",
     "GroupNameChangedEvent",
     "GroupMessageRecalledEvent",
+    "GroupMemberJoinedEvent",
+    "GroupNudgeEvent",
+    "GroupMemberLeaveEvent",
+    "GroupMemberPermissionChangeEvent",
+    "GroupLuckyCharacterNewEvent",
+    "GroupLuckyCharacterInitEvent",
+    "GroupLuckyCharacterOpenedEvent",
+    "GroupLuckyCharacterClosedEvent",
+    "GroupLuckyCharacterChangedEvent",
+    "GroupRedbagEvent",
+    "JoinGroupRequestEvent",
+    "TransferGroupEvent",
+    "OtherClientChangedEvent"
 ]
